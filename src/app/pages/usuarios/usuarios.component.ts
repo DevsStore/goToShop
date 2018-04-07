508b5e7eef429e83891d6c7c3c978c954c72cd7d
Jolver Andre <36704606+JolverAndre@users.noreply.github.com>
2018-04-06 21:08:32 -0500

    asdasd

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -2,26 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { GrupoService } from '../../services/grupo.service';
 
 @Component({
-  selector: 'app-usuarios',
-  templateUrl: './usuarios.component.html',
-  styles: []
+    selector: 'app-usuarios',
+    templateUrl: './usuarios.component.html',
+    styles: []
 })
 export class UsuariosComponent implements OnInit {
+    data: Array<Usuario> = []
 
 
+    constructor(public grupoServices: GrupoService) { }
 
-  constructor(public grupoServices: GrupoService) { }
+    ngOnInit() {
+        this.cargarUsuario();
 
-  ngOnInit() {
-      this.cargarUsuario();
-  }
+    }
 
-  cargarUsuario() {
-    this.grupoServices.cargarUsuario()
-              .subscribe( resp => {
-                console.log(resp);
-              });
+    cargarUsuario() {
+        this.grupoServices.cargarUsuario()
+            .subscribe((resp:any) => {
+                //la variable datos se llena con el resultado de la peticion
+                this.data=resp;
+            });
 
-  }
+    }
 
+}
+//creas la interface, una interface es parecida a un modelo x q basicamente modela el dato, ahora la ventaja es q no usas get ni set
+interface Usuario{
+  id: number;
+  fullname: string;
+  telefono:string;
+  direccion:string;
+  di:string;
+  email:string;
+  tipo:string;
+  image_url:string;
+  created_at:string;
+  updated_at:string;
 }
